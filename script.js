9dba061337619f1e9e755797a5a7d494ace6571b
Evaly Adam <152657385+Evalyadam@users.noreply.github.com>
2024-02-08 18:48:31 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,65 @@
+function freddy_fazbear() {
+    var audio = new Audio("Sounds/cyzo-fready-fazbear.mp3");
+    audio.play();
+  }
+  
+
+function HEE_HEE() {
+    var audio = new Audio("Sounds/HEE-HEE.mp3");
+    audio.currentTime = 1.4;
+    audio.play();
+  }
+  
+
+  function YOU_ARE_MINE() {
+    var audio = new Audio("Sounds/you-are-mine.mp3");
+    audio.play();
+  }
+
+  function OH_GOD_NO() {
+    var audio = new Audio("Sounds/OH-GOD-NO-NOT-AGAIN.mp3");
+    audio.play();
+  }
+  
+
+  function fnaf_2_ambience(){
+    var audio = new Audio("Sounds/bdumbudum.mp3");
+    audio.play();
+  }
+
+  function boom(){
+    var audio = new Audio("Sounds/boom.mp3");
+    audio.play();
+  }
+
+  function glitch(){
+    var audio = new Audio("Sounds/glitch.mp3");
+    audio.play();
+  }
+
+  function correct_robotic(){
+    var audio = new Audio("Sounds/robotic-correct.mp3");
+    audio.play();
+  }
+
+  function robotic_wrong(){
+    var audio = new Audio("Sounds/wrong-robotic.mp3");
+    audio.play();
+  }
+
+  function four_wolves(){
+    var audio = new Audio("Sounds/four-wolves.mp3");
+    audio.play();
+  }
+
+  function click_sfx() {
+    var audio = new Audio("Sounds/click.mp3");
+    audio.play();
+  }
+  
+
+  function squeak() {
+    var audio = new Audio("Sounds/squeak.mp3");
+    audio.play();
+  }
+  
